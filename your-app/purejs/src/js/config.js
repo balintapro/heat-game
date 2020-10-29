3e6a53702270a3974874934e1f9647c453d4bb86
Bálint <abalint88@gmail.com>
2020-10-29 16:24:14 +0100

    gitignore

--- your-app/purejs/src/js/config.js
+++
@@ -1,2 +0,0 @@
-export const APIKEY = "19b2288059c1f9dd1164ecaf9ca7b295";
-export const mapboxAccessToken = "pk.eyJ1IjoiYWJhbGludDg4IiwiYSI6ImNqdWUwamFieTA0amU0NHF4dGF0NXA0ajUifQ.cxLl7qyDfSRk-GM62qT1xA";
